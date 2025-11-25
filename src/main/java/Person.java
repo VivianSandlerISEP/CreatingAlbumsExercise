e033417d890977656335f8bb466ed162881b6003
edu_link <1251996@isep.ipp.pt>
2025-11-25 18:35:04 +0000

    changes to Person

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,2 +1,20 @@
 public class Person {
+
+    //variables
+    private String name;
+    private String stageName;
+
+    //constructor
+    public Person(String name, String stageName) {
+        this.name = name;
+        this.stageName = stageName;
+    }
+    //getter_setter for Name
+    public String getName (){
+        return this.name;
+    }
+    //getter_setter for stageName
+    public String getStageName (){
+        return this.stageName;
+    }
 }
