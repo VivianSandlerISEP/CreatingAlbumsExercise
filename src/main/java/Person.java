bd63bb0a4f8fed0ed955c806270c70493319dff0
VivianSandlerISEP <1252010@isep.ipp.pt>
2025-11-25 18:32:36 +0000

    Added tests in PersonTest

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,2 +1,23 @@
 public class Person {
+
+    //variables
+    private String name;
+    private String stageName;
+
+    //constructor
+    public Person(String name, String stageName) {
+        this.name = name;
+        this.stageName = stageName;
+    }
+
+    // Person methods
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getStageName(){
+        return this.stageName;
+    }
+
 }
