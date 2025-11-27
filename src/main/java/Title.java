acd0a129512b9202b4c8e0890e51ba19f8fc00ce
Raj <1252003@isep.ipp.pt>
2025-11-27 11:48:01 +0000

    Created Title class branch and added code

---
+++ src/main/java/Title.java
@@ -0,0 +1,17 @@
+// Title class
+    public class Title {
+        private String title;
+
+
+        public Title(String title) {
+            this.title = title;
+
+        }
+
+        public String getTitle() {
+            return this.title;
+        }
+
+        }
+
+
