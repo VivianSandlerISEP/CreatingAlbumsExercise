a0398bad13c53d145e25cffa9c4b7c1cd2701b53
Hugo Durao <1251933@isep.ipp.pt>
2025-11-27 13:09:12 +0000

    Finished Album class

--- src/main/java/Album.java
+++ src/main/java/Album.java
@@ -1,2 +1,52 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Album {
+
+    private String albumTitle;
+    private Performer albumPerformer;
+    private ArrayList<Song> songs;
+    private ReleaseDate releaseDate;
+    private Label albumLabel;
+
+    public Album() {
+
+        this.albumTitle = albumTitle;
+        this.albumPerformer = albumPerformer;
+        this.releaseDate = releaseDate;
+        this.albumLabel = albumLabel;
+        this.songs = new ArrayList<>();
+
+    }
+
+    public String getAlbumTitle() {
+        return albumTitle;
+    }
+
+    public void addSongs(Song song) {
+        this.songs.add(song);
+    }
+
+    public void removeSongs(Song song) {
+        this.songs.remove(song);
+    }
+
+    public ArrayList<Song> getSongs() {
+        return songs;
+    }
+
+    public int getNumberOfSongs() {
+        return songs.size();
+    }
+
+    public ReleaseDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    @Override
+
+    public String toString(){
+        return "Album: " + albumTitle + ", Performer:  " + albumPerformer + ", ReleaseDate: " + releaseDate.getYear() + ", Label: " + albumLabel + ", Number of songs: " + songs.size();
+
+    }
 }
