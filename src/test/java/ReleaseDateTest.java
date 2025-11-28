23bfefb92f723adcf43784328645a5de30b08e8b
Hugo Durao <1251933@isep.ipp.pt>
2025-11-28 11:10:03 +0000

    ReleaseDate Test

--- src/test/java/ReleaseDateTest.java
+++ src/test/java/ReleaseDateTest.java
@@ -1,5 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReleaseDateTest {
 
+    @Test
+    void getReleaseDateReturnsValueFromConstructor() {
+
+        ReleaseDate releaseDate = new ReleaseDate(LocalDate.of(2025, 12, 25));
+
+        LocalDate expected = LocalDate.of(2025, 12, 25);
+        LocalDate actual = releaseDate.getReleaseDate();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetYear() {
+
+        ReleaseDate releaseDate = new ReleaseDate(LocalDate.of(2026, 1, 1));
+
+        int actual = releaseDate.getYear();
+        assertEquals(2026, actual);
+    }
 }
