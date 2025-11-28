4f4ffd1a377e5ae9ee22b8f63717b42263633764
Hugo Durao <1251933@isep.ipp.pt>
2025-11-28 11:07:10 +0000

    ReleaseDate class

--- src/main/java/ReleaseDate.java
+++ src/main/java/ReleaseDate.java
@@ -1,2 +1,19 @@
+import java.time.LocalDate;
+
 public class ReleaseDate {
+
+    private final LocalDate releaseDate;
+
+    public ReleaseDate(LocalDate releaseDate) {
+
+        this.releaseDate = releaseDate;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public int getYear() {
+        return releaseDate.getYear();
+    }
 }
